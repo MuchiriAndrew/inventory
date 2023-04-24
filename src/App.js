0798016a0e539abd57dc1c7434f77dde514b98d0
MuchiriAndrew <kariukia225@gmail.com>
2023-04-24 10:38:50 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import "./App.css";
+import SearchBar from "./SearchBar";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState ({});
+
+  const updateData = (searchParams) => {
+    setData(searchParams);
+  }
+
   return (
     <div className="App">
+      <SearchBar callback = {updateData}/>
+      <p>Name: {"name" in data ? data["name"] : "No Data to display"}</p>
+
+      <p>Max price: {"price" in data ? data["price"] : "No Data to display"}</p>
+
+      <p>Type: {"type" in data ? data["type"] : "No Data to display"}</p>
+
+      <p>Brand: {"brand" in data ? data["brand"] : "No Data to display"}</p>
+
     </div>
   );
 }
